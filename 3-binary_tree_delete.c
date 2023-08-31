41c466ea0e579890a7beb5d2c3cdec614453b2c9
vierukundo <vierukundo@gmail.com>
2023-08-31 13:27:47 -0700

    ..k

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,14 @@
+/**
+ * binary_tree_delete - function that deletes an entire binary tree
+ * @tree: a pointer to the root node of the tree to delete
+ * Return: nothing
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	if (tree != NULL)
+		free(tree);
+}
